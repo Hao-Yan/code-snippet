4601a570afbcfa80ea6a6b3f296875acf50a0e86
Hao <hayan@microsoft.com>
2020-06-01 09:35:52 -0700

    cxx11_abi_test more findings

--- src/cxx11_abi_test/test.cpp
+++ src/cxx11_abi_test/test.cpp
@@ -2,9 +2,17 @@
 #include <string>
 using namespace std;
 
+std::string result = "11111111";
+
 std::string test_return_string()
 {
-    std::string result = "11111111";
+    // When compliled with _GLIBCXX_USE_CXX11_ABI=0, if we don't modify the result, then
+    // the result returned will use result's data directly, no copy will happen. That' the
+    // copy-on-write. But this does not confirm to the c++ standard, so the new ABI drop this.
+    // when _GLIBCXX_USE_CXX11_ABI=1, no matter modify or not, a copy will happen.
+    // https://stackoverflow.com/questions/12199710/legality-of-cow-stdstring-implementation-in-c11
+    // result[0] = '0';
+    cout << "Address in test_return_string: " << (void *)result.data() << endl;
     return result;
 }
 
@@ -12,7 +20,7 @@ int main()
 {
     std::string result = test_return_string();
 
-    cout << (void *)result.data() << endl;
+    cout << "Address in main: " << (void *)result.data() << endl;
     for (int i = 0; i < 10; ++i)
     {
         cout << "hello world" << endl;
